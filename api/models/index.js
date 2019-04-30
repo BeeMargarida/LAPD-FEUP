1b495fed3267ed903a2d72af0d772d4084c30fa3
BeeMargarida <up201505505@gcloud.fe.up.pt>
2019-04-30 14:34:51 +0100

    updated README

--- api/models/index.js
+++ api/models/index.js
@@ -6,5 +6,7 @@ mongoose.connect(DB_CONNECTION, {
     useNewUrlParser: true
 });
 
+console.log(DB_CONNECTION);
+
 module.exports.User = require("./user");
 module.exports.History = require("./history");
