0060177dc7189c5ed2a5a10600758d1c3cbe614e
BeeMargarida <up201505505@gcloud.fe.up.pt>
2019-05-11 17:35:06 +0100

    api livestream and alarm working, but with some ocasional segmentation faults. Trying to solve

--- api/controllers/alarm.js
+++ api/controllers/alarm.js
@@ -1,52 +1,73 @@
-const express = require("express");
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server, { origins: '*:*' });
 const cv = require('opencv4nodejs');
-
 const { createHistory } = require("./history");
 const { runWebcamObjectDetect } = require("./detection");
 const { opencvSettings } = require('../config/config');
+
 
 let alarm = null;
 let alarmOn = false;
 let livestream = false;
 let wCap = null;
+let io = null;
 
-exports.startAlarm = async function (req, res, next) {
+function startSocketConnection() {
+    if (io == null) {
+        const server = require("../index").server;
+        io = require('socket.io')(server);
+    }
+    return io;
+}
 
-    alarmOn = true;
-    if(wCap == null) {
+function getCamera() {
+    if (wCap == null) {
         wCap = new cv.VideoCapture(opencvSettings.camPort);
     }
+}
 
-    alarm = runWebcamObjectDetect(wCap, alarmOn, livestream, io, req, res, next);
+function stopCamera() {
+    if(wCap != null) {
+        wCap.release();
+        wCap = null;
+    }
+}
+
+exports.startAlarm = function (req, res, next) {
+    alarmOn = true;
+
+    getCamera();
+
+    if (livestream) {
+        clearInterval(alarm);
+    }
+    alarm = runWebcamObjectDetect(wCap, null, alarmOn, livestream, req.user)
 
     createHistory({
         type: "Turn On Alarm",
-        imagePath: null, //TODO: Change later
+        imagePath: null,
         user: req.user
-    }, res, next)
+    })
         .then((result) => {
             return res.status(200).json(result);
-        });
+        })
+        .catch((err) => next(err))
+
 }
 
 exports.stopAlarm = async function (req, res, next) {
     if (alarmOn) {
-        
-        if(!livestream){
+
+        // If livestream is off, clear everything
+        if (!livestream) {
             clearInterval(alarm);
             alarm = null;
-            wCap.release();
-            wCap = null;
+            stopCamera();
         }
 
         alarmOn = false;
 
         createHistory({
             type: "Turn Off Alarm",
-            imagePath: null, //TODO: Change later
+            imagePath: null,
             user: req.user
         }, res, next)
             .then((result) => {
@@ -57,7 +78,7 @@ exports.stopAlarm = async function (req, res, next) {
 }
 
 exports.getAlarmState = async function (req, res, next) {
-    if (alarm == null) {
+    if (!alarmOn) {
         return res.status(200).json({ alarm: false })
     }
     return res.status(200).json({ alarm: true })
@@ -65,26 +86,36 @@ exports.getAlarmState = async function (req, res, next) {
 
 exports.getLiveStream = async function (req, res, next) {
 
-    if(wCap == null) {
-        wCap = new cv.VideoCapture(opencvSettings.camPort);
-    }
-    
-    livestream = true;
+    if (!livestream) {
 
-    if(alarm != null){
-        clearInterval(alarm);
+        getCamera();
+
+        livestream = true;
+
+        // If alarm was on, restart it
+        if (alarmOn) {
+            clearInterval(alarm);
+        }
+        startSocketConnection();
+        io.on('connection', (socket) => { alarm = runWebcamObjectDetect(wCap, socket, alarmOn, livestream, req.user) });
     }
 
-    alarm = runWebcamObjectDetect(wCap, alarmOn, livestream, io, req, res, next); // Review this!
     return res.status(200).json({});
 }
 
 exports.stopLiveStream = async function (req, res, next) {
+    console.log("STOP LIVESTREAM");
     livestream = false;
     clearInterval(alarm);
-    alarm = runWebcamObjectDetect(wCap, alarmOn, livestream, io, req, res, next); // Review this!
+
+    // If alarm is on, restart it
+    if(alarmOn){
+        alarm = runWebcamObjectDetect(wCap, null, alarmOn, livestream, req.user);
+    }
+    else {
+        alarm = null;
+        stopCamera();
+    }
     return res.status(200).json({});
 }
-
-server.listen(5555)
 
--- flutter_app/lib/configs.dart
+++ flutter_app/lib/configs.dart
@@ -1,6 +1,6 @@
 class Configs{
-  static final String API_HOST = "178.166.11.252:3000";
+  static final String API_HOST = "192.168.1.71:3000";
   static final String API_PATH = '/api/';
-  static final String LIVESTREAM_HOST = "178.166.11.252";
-  static final int LIVESTREAM_PORT = 5555;
+  static final String LIVESTREAM_HOST = "192.168.1.71";
+  static final int LIVESTREAM_PORT = 3000;
 }
